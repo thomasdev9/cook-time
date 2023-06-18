6272781d9ee1508efbd5d9b618f8571453354e12
thomas <makis7olymp@yahoo.gr>
2023-06-18 21:01:22 +0300

    React router dom

---
+++ src/router/router.js
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Wrapper from '../components/wrapper';
+import Home from '../pages/home';
+import Categories from '../pages/categories';
+import Recipes from '../pages/recipes';
+import SingleRecipe from '../pages/single-recipe';
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Wrapper />}>
+          <Route index element={<Home />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/recipes" element={<Recipes />} />
+          <Route path="/recipe" element={<SingleRecipe />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
