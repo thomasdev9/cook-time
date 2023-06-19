17713b6bbf1e6018448fbb917fe7c85fa94a95f7
thomas <makis7olymp@yahoo.gr>
2023-06-19 17:13:33 +0300

    Main search bar

---
+++ src/components/search-bar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { InputAdornment, TextField } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+
+const styles = {
+  borderRadius: '25px',
+};
+
+function SearchBar({ setSearchQuery }) {
+  return (
+    <form>
+      <TextField
+        id="search-bar"
+        className="text"
+        onInput={(e) => {
+          setSearchQuery(e.target.value);
+        }}
+        placeholder="Search any recipe"
+        variant="outlined"
+        InputProps={{
+          sx: styles,
+          endAdornment: (
+            <InputAdornment position="end">
+              <SearchIcon />
+            </InputAdornment>
+          ),
+        }}
+        size="small"
+        fullWidth
+      />
+    </form>
+  );
+}
+
+export default SearchBar;
