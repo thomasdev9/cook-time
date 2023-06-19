a6197036c646af85c675165385199de441810733
thomas <makis7olymp@yahoo.gr>
2023-06-19 16:37:06 +0300

    Main header

--- src/components/wrapper.js
+++ src/components/wrapper.js
@@ -3,11 +3,15 @@ import { Outlet } from 'react-router-dom';
 import CssBaseline from '@mui/material/CssBaseline';
 import { Container } from '@mui/material';
 
+const wrapperStyles = {
+  paddingTop: 5,
+};
+
 function Wrapper() {
   return (
     <React.Fragment>
       <CssBaseline />
-      <Container maxWidth="sm">
+      <Container maxWidth="sm" sx={wrapperStyles}>
         <Outlet />
       </Container>
     </React.Fragment>
