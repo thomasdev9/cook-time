95363184b832b98dbea2d8584697a3c2905343ea
thomas <makis7olymp@yahoo.gr>
2023-06-22 16:51:31 +0300

    Content loader

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,6 +6,8 @@ import CreateCarousel from '../features/components/create-carousel';
 import { categoriesChips, getRecipesCards } from '../shared/setup/home';
 import { fetchRecommendations } from '../redux/slices/recommendationsSlice';
 import { isEmpty } from 'lodash';
+import CatalogLoader from '../features/content-loaders/catalog-loader';
+import CategoriesLoader from '../features/content-loaders/categories-loader';
 
 function Home() {
   const [searchQuery, setSearchQuery] = useState('');
@@ -15,6 +17,7 @@ function Home() {
     const data = state.recommendations;
     return [data.recommended, data.recently, data.popular, data.trending];
   });
+  const isLoading = useSelector((state) => state.recommendations.loading);
 
   useEffect(() => {
     dispatch(fetchRecommendations());
@@ -30,18 +33,26 @@ function Home() {
           What would you like to cook today?
         </Typography>
         <SearchBar setSearchQuery={setSearchQuery} />
-        <CreateCarousel title="Categories" slides={categoriesChips} slidesPerView="auto" spaceBetween={5} />
-        {recommendations?.map(
-          (rec, index) =>
-            !isEmpty(rec.data) && (
-              <CreateCarousel
-                title={rec?.title}
-                slides={getRecipesCards(rec?.data)}
-                slidesPerView="auto"
-                spaceBetween={10}
-                key={index}
-              />
-            )
+        {isLoading ? (
+          <CategoriesLoader />
+        ) : (
+          <CreateCarousel title="Categories" slides={categoriesChips} slidesPerView="auto" spaceBetween={5} />
+        )}
+        {isLoading ? (
+          <CatalogLoader />
+        ) : (
+          recommendations?.map(
+            (rec, index) =>
+              !isEmpty(rec.data) && (
+                <CreateCarousel
+                  title={rec?.title}
+                  slides={getRecipesCards(rec?.data)}
+                  slidesPerView="auto"
+                  spaceBetween={10}
+                  key={index}
+                />
+              )
+          )
         )}
       </Grid>
     </Box>
