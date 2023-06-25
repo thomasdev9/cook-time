2b540823b891f8b3cc15f62a4ee5a0b53aec6e37
thomas <makis7olymp@yahoo.gr>
2023-06-25 15:32:50 +0300

    46-search-recipes

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
-import categoriesReducer from './slices/categoriesSlice';
 import recommendationsReducer from './slices/recommendationsSlice';
 
 const reducer = combineReducers({
-  categories: categoriesReducer,
   recommendations: recommendationsReducer,
 });
 
